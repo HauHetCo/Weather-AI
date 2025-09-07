275508963cc39cc52041214c0a874da1feae9eba
HauHet.Co <63187237+HauHetCo@users.noreply.github.com>
2025-09-08 01:52:50 +0300

    Update index.php

--- Weather  AI/index.php
+++ Weather  AI/index.php
@@ -1,1 +1,24 @@
 <?php include("html.php"); ?>
+<!DOCTYPE html>
+
+<!-- 
+//////////////////////////////////////////////////////
+
+HauHet plc.
+hauhet.co
+
+We are HauHet
+
+Headquarters
+HauHet plc. – Xolo Go OU
+Private Limited Company
+
+Kalasadama 4
+10415 – Tallinn
+Estonia
+
+//////////////////////////////////////////////////////
+-->
+
+<html lang='en'>
+
